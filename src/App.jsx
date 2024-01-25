bf49eb5cd3eb72734282f2968586123bdaa86d1c
Sujal Shresth <sujshrest@example.com>
2024-01-25 09:51:14 +0530

    Assignment Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   return (
     <>
-      <div>
-        
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-          <img src={reactLogo} className="logo react" alt="React logo" />
-      </div>
-      <h3>Vite + React</h3>
       <h1>Hello Kalvium</h1>
     </>
   )
